cc61a2b78b66d8cd316689307148492034a8d67c
PepeMaitreDesCodes <pierre-paul.hamon@bell.net>
2018-01-31 10:28:46 -0500

    Initial commit

---
+++ Assets/scripts/Bouger_Balle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouger_Balle : MonoBehaviour {
+
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(10.0f, 0.0f, 0.0f);
+        rb.AddForce(movement);
+    }
+}
